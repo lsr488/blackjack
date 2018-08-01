c9d695010750cdd9c24e8574d2027951d11891ca
Lisa <lsr488@gmail.com>
2018-08-01 00:25:16 -0400

    Added dealer stand/hit logic.

--- blackjack.js
+++ blackjack.js
@@ -7,7 +7,7 @@
 	// deal second card to each player (face up), then dealer (face down)
 	// player
 		// player can "hit" or "stand"
-			// can hit multiple times per term, until stand
+			// can hit multiple times per turn, until stand
 		// if total > 21, goes bust (loses)
 		// ace can be 1 or 11, depending on other cards/total
 	// dealer play
@@ -32,13 +32,20 @@
 	// if dealer's face-up card is a ten-card, deal looks at face-down card to see if natural
 		// else doesn't look at face-down card until it's dealer's turn again
 
+var playerHand = document.querySelector("#player-hand");
+var playerTotal = document.querySelector("#player-total");
+var playerUpdate = document.querySelector("#player-update");
+var dealerHand = document.querySelector("#dealer-hand");
+var dealerTotal = document.querySelector("#dealer-total");
+var dealerUpdate = document.querySelector("#dealer-update");
+
 var deck = {
 	cards: [],
-	numOfCards: 13,
+	numOfCards: 10,
 }
 
-// maybe player1 should be an object that holds ALL players...?
-var player1 = {
+// maybe player should be an object that holds ALL players...?
+var player = {
 	hand: [],
 	isBust: false,
 	isWin: false,
@@ -54,12 +61,12 @@ var dealer = {
 function varSetUp() {
 	cards = deck.cards;
 	numOfCards = deck.numOfCards;
-	pHand = player1.hand;
-	// pSum = player1.sum;
-	// pIsBust = player1.isBust;
-	pIsWin = player1.isWin;
-	dHand = dealer.hand;
-	dSum = dealer.sum;
+	// pHand = player.hand;
+	// pSum = player.sum;
+	// pIsBust = player.isBust;
+	pIsWin = player.isWin;
+	// dHand = dealer.hand;
+	// dSum = dealer.sum;
 	dIsBust = dealer.isBust;
 	dIsWin = dealer.isWin;
 }
@@ -81,63 +88,85 @@ function genDeck() {
 	tempCards.forEach(function(i) {
 		cards.push(i);
 	})
+	console.log("deck:", cards);
 }
 
 function initialDeal() {
-	dealCard(pHand);
-	dealCard(dHand);
-	dealCard(pHand);
-	dealCard(dHand);
+	dealCard(player);
+	dealCard(dealer);
+	dealCard(player);
+	dealCard(dealer);
 	// displayHands();
 }
 
-function dealCard(hand) {
+function dealCard(turn) {
+	var hand = turn.hand;
 	hand.push(cards.pop());
 }
 
 function displayHands() {
-	addCards(pHand);
-	addCards(dHand);
-	console.log("player1:", pHand.join(", "), `(total: ${pSum})`);
-	console.log("dealer:", dHand.join(", "), `(total: ${dSum})`);	
+	addCards(player);
+	addCards(dealer);
+	console.log("player:", player.hand.join(", "), `(total: ${player.sum})`);
+	console.log("dealer:", dealer.hand.join(", "), `(total: ${dealer.sum})`);	
+	updatePlayerDisplays();
+	updateDealerDisplays();
 }
 
-function addCards(hand) {
+function addCards(turn) {
+	var hand = turn.hand;
 	sum = hand.reduce(function(accum, currVal) {
 		return accum + currVal;
 	}, 0);
 
-	// need to re-sync dealer.sum and dSum
-	if(hand == dHand) {
-		dealer.sum = sum;
-		dSum = sum;	
-	}
+	// if(turn == dealer) {
+		if(hand[0] === 1 || hand[1] === 1) {
+			sum += 10;
+			console.log("ace now 11 in dealer hand");
+			if(sum > 21) {
+				sum -= 10;
+				console.log("ace now 1 in dealer hand");
+			}
+		}
+		turn.sum = sum;
+		// dSum = sum;	
+	// }
 
-	if(hand == pHand) {
-		player1.sum = sum;
-		pSum = sum;	
-	}
+	// if(turn == player) {
+	// 	player.sum = sum;
+	// 	// pSum = sum;	
+	// }
 }
 
-function hit(hand) {
-	if(hand == pHand) {
-		dealCard(hand);
-		addCards(hand);
-		console.log("player1:", hand.join(", "), `(total: ${pSum})`);
-		checkScore(player1);
+function hit(turn) {
+	var hand = turn.hand;
+
+	if(turn == player) {
+		dealCard(player);
+		addCards(player);
+		console.log("Dealt 1 card to player.");
+		console.log("player:", player.hand.join(", "), `(total: ${player.sum})`);
+		updatePlayerDisplays();
+		playerUpdate.innerText = "Dealt 1 card to player.";
+		checkScore(player);
 	}
-	if(hand == dHand) {
-		if(dSum >= 17) {
+	if(turn == dealer) {
+		if(dealer.sum >= 17) {
 			// if total >= 17, dealer stands
-			console.log(dSum + " >= 17: dealer stands");
-			console.log("dealer:", hand.join(", "), `(total: ${dSum})`);	
+			console.log(dealer.sum + " >= 17: dealer stands");
+			console.log("dealer:", dealer.hand.join(", "), `(total: ${dealer.sum})`);	
+			dealerUpdate.innerText = "Dealer stands.";
 			checkScore(dealer);
-		} else if(dSum <= 16) {
+		} else if(dealer.sum <= 16) {
 			// if total <= 16, dealer hits until 17 >= 17
-			console.log(dSum + " <= 16: dealer hits until 17 or greater");
-			dealCard(hand);
-			addCards(hand);
-			console.log("dealer:", hand.join(", "), `(total: ${dSum})`);	
+			console.log(dealer.sum + " <= 16: dealer hits until 17 or greater");
+			dealCard(dealer);
+			addCards(dealer);
+			console.log("Dealt 1 card to dealer.");
+			console.log("dealer:", dealer.hand.join(", "), `(total: ${dealer.sum})`);	
+			updateDealerDisplays();
+			dealerUpdate.innerText = "Dealt 1 card to dealer.";
+
 			checkScore(dealer);
 		}
 		// if ace card, counts as 11 if brings total >= 17 (and dealer must stand then)
@@ -145,16 +174,36 @@ function hit(hand) {
 	}
 }
 
-function checkScore(player) {
-	if(player.sum == 21) {
+function checkScore(turn) {
+	if(turn.sum == 21) {
 		console.log("Blackjack!");
-		player.isWin = true;
-	} else if(player.sum > 21) {
+		turn.isWin = true;
+		if(turn == player) {
+			playerUpdate.innerText = "Blackjack!"
+		} else {
+			dealerUpdate.innerText = "Blackjack!"
+		}
+	} else if(turn.sum > 21) {
 		console.log("Bust!");
-		player.isBust = true;
+		turn.isBust = true;
+		if(turn == player) {
+			playerUpdate.innerText = "Bust!"
+		} else {
+			dealerUpdate.innerText = "Bust!"
+		}
 	} else {
 		console.log("score < 21");
 	}
+}
+
+function updatePlayerDisplays() {
+	playerHand.innerText = player.hand.join(", ");
+	playerTotal.innerText = player.sum;	
+}
+
+function updateDealerDisplays() {
+	dealerHand.innerText = dealer.hand.join(", ");
+	dealerTotal.innerText = dealer.sum;
 }
 
 function checkWinner() {
@@ -167,6 +216,6 @@ initialDeal();
 // addCards(dHand);
 // addCards(pHand);
 displayHands();
-checkScore(player1);
-checkScore(dealer);
+// checkScore(player);
+// checkScore(dealer);
 
